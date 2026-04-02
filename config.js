cafaf8bfc117f95c003d34ce3174a0e39373908f
Nguyễn Đức Lộc <89655882+nguyenducloc203@users.noreply.github.com>
2026-04-02 22:19:03 +0700

    Update titles and messages in config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'không ăn cay nữa nào ',
+    title: 'Không nghĩ linh tinh nữa nhá yêu <3',
     desc: '',
     btnYes: 'Vângg🫶 ',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Cho anh một cơ hội nhá <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối mai anh sang ebe chơi nhá ',
+    reply: 'Vâng <33333333',
+    mess: 'Anh biết mà 🥰😘😘',
+    messDesc: 'Chúc vịu ơ ngày mai thiii tốt nhá <3333',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/Loc.nguyen.gaming.13.09' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
