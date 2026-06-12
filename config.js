aadc6f4496cfbe801e58ff2e2360c3ecfa74fcc6
Nguyễn Đức Lộc <89655882+nguyenducloc203@users.noreply.github.com>
2026-06-12 23:14:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,11 +9,11 @@ const CONFIG = {
     desc: '',
     btnYes: 'Vângg🫶 ',
     btnNo: 'Không, Anh trai à :3',
-    question:'Cho anh một cơ hội nhá <3',
+    question:'mai anh về với vợ nhá <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Vâng <33333333',
     mess: 'Anh biết mà 🥰😘😘',
-    messDesc: 'mai anh về với vợ nhóoooo <3333',
+    messDesc: 'iu em <3333',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/Loc.nguyen.gaming.13.09' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
